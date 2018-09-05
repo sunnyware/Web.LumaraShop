6a65a43f3ad98867729aa9b788af740e1226bcb7
stephanblaurock <stephan.blaurock@outlook.de>
2018-09-05 23:59:03 +0200

    aktiv-gg wettbewerb

--- src/app/views/statistik/statistik.component.ts
+++ src/app/views/statistik/statistik.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import {LumaraService} from '../../service/lumara_service';
 import { LumaraServiceCommands } from '../../service/lumara_service_commands';
-import { StatistikJahresspiegel, StatistikJahresstatistik, MyChartItem } from '../../models/statistik';
+import { StatistikJahresspiegel, StatistikJahresstatistik, MyChartItem, GastgeberStatistikItem } from '../../models/statistik';
 import {Router} from '@angular/router';
 import notify from 'devextreme/ui/notify';
 
@@ -11,6 +11,8 @@ import notify from 'devextreme/ui/notify';
   styles: []
 })
 export class StatistikComponent implements OnInit {
+  gastgeberStatistikItems: GastgeberStatistikItem[] = undefined;
+  showEmptyGastgeberList = false;
   chartDataJahresspiegel: StatistikJahresspiegel = undefined;
   chartDataJahresstatistik: StatistikJahresstatistik = undefined;
 
@@ -19,10 +21,46 @@ export class StatistikComponent implements OnInit {
   }
 
   ngOnInit() {
+    this.reloadGastgeberStatistik();
     this.reloadJahresspiegel();
     this.reloadJahresstatistik();
   }
 
+  reloadGastgeberStatistik() {
+    // Ewertowski IDPersonalakte = 7951858
+    this.lumaraService
+      .doCommand(
+        LumaraServiceCommands.GetGastgeberStatistik(
+          0,
+          0,
+          0,
+          new Date('2017-06-01'),
+          new Date('2018-05-31'),
+          3,
+          1200,
+          1
+        )
+      )
+      .subscribe(data => {
+        if (data.ReturnCode === 200) {
+          // console.log('Ich bekam vom Server folgende Daten: ');
+          // console.log(data.ReturnMessage + '\r\n' + data.ReturnValue);
+          // console.log(data.ReturnValue);
+          this.gastgeberStatistikItems = JSON.parse(data.ReturnValue); // JSON.parse(data.ReturnValue);
+          if (this.gastgeberStatistikItems && this.gastgeberStatistikItems.length > 0) {
+            this.showEmptyGastgeberList = false;
+          } else {
+            this.showEmptyGastgeberList = true;
+          }
+          // this.gastgeberStatistik.GastgeberList = JSON.parse(this.gastgeberStatistik.GastgeberList.toString());
+          // console.log(this.gastgeberStatistik);
+        } else {
+          console.log(data.ReturnMessage + '\r\n' + data.ReturnValue);
+          // this.router.navigate(['/login']);
+          notify(data.ReturnMessage);
+        }
+      });
+  }
   reloadJahresspiegel() {
     this.lumaraService.doCommand(LumaraServiceCommands.GetJahresspiegel(0, false)).subscribe(
       data => {
